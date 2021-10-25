28d3225d54c5736b6e3908b74463b334bef9458b
HEKERWIBU <91865135+HEKERWIBU@users.noreply.github.com>
2021-10-25 09:59:45 +0700

    Update and rename fact.js to facts.js

--- commands/fun/fact.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-name: "facts",
-code: `$addTimestamp
-$image[https://dinosaur.ml/overlay/facts/?text=$replaceText[$message; ;+]]
-$title[Well that's true]
-$color[RANDOM]`
-});
---
+++ commands/fun/facts.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+name: "facts",
+code: `
+$attachment[https://api.cool-img-api.ml/facts?text=$message]`
+}
